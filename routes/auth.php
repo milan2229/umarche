6a16e885c0c45d2443c9cbdfca0a97f124438202
milan2229 <milan_1899.22@icloud.com>
2021-10-02 00:27:54 +0900

    [update] admin,owner,userのマルチログイン終了しました。

--- routes/auth.php
+++ routes/auth.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\UserAuth\AuthenticatedSessionController;
-use App\Http\Controllers\UserAuth\ConfirmablePasswordController;
-use App\Http\Controllers\UserAuth\EmailVerificationNotificationController;
-use App\Http\Controllers\UserAuth\EmailVerificationPromptController;
-use App\Http\Controllers\UserAuth\NewPasswordController;
-use App\Http\Controllers\UserAuth\PasswordResetLinkController;
-use App\Http\Controllers\UserAuth\RegisteredUserController;
-use App\Http\Controllers\UserAuth\VerifyEmailController;
+use App\Http\Controllers\User\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\User\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\User\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\User\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\User\Auth\NewPasswordController;
+use App\Http\Controllers\User\Auth\PasswordResetLinkController;
+use App\Http\Controllers\User\Auth\RegisteredUserController;
+use App\Http\Controllers\User\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
@@ -41,24 +41,24 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.update');
 
 Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
-                ->middleware('auth')
+                ->middleware('auth:users')
                 ->name('verification.notice');
 
 Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth:users', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['auth:users', 'throttle:6,1'])
                 ->name('verification.send');
 
 Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->middleware('auth')
+                ->middleware('auth:users')
                 ->name('password.confirm');
 
 Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-                ->middleware('auth');
+                ->middleware('auth:users');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+                ->middleware('auth:users')
                 ->name('logout');
