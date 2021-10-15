91a81d3a863b8e9e95326a366fd0a1f0adce1f0c
milan2229 <milan_1899.22@icloud.com>
2021-10-15 22:27:59 +0900

    [add] no_image画像追加

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -32,8 +32,8 @@ class ShopController extends Controller
     }
 
     public function index() {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        // $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', Auth::id())->get();
         // ショップモデルでオーナーidで検索して、引っかかった物をgetする。
 
         return view('owner.shops.index', compact('shops'));
