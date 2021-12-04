ccd85d232b72b5b6a6e6ec2c3f2ef1d5a5bd78e0
milan2229 <milan_1899.22@icloud.com>
2021-12-05 00:47:40 +0900

    [update]ソート機能実装

--- app/Constants/Common.php
+++ app/Constants/Common.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Constants;//フォルダの場所
+namespace App\Constants; //フォルダの場所
 
 class Common
 {
@@ -10,6 +10,18 @@ class Common
   const PRODUCT_LIST = [
     'add' => self::PRODUCT_ADD,
     'reduce' => self::PRODUCT_REDUCE
-  ];//連想配列で管理する。クラスの中でconstを選択するにはselfをつける。
-}
+  ]; //連想配列で管理する。クラスの中でconstを選択するにはselfをつける。
 
+  const ORDER_RECOMMEND = '0';
+  const ORDER_HIGHER = '1';
+  const ORDER_LOWER = '2';
+  const ORDER_LATER = '3';
+  const ORDER_OLDER = '4';
+  const SORT_ORDER = [
+    'recommend' => self::ORDER_RECOMMEND,
+    'higherPrice' => self::ORDER_HIGHER,
+    'lowerPrice' => self::ORDER_LOWER,
+    'later' => self::ORDER_LATER,
+    'older' => self::ORDER_OLDER
+  ];
+}
--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -14,11 +14,23 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('item'); //routeのweb.phpの{item}の箇所
+            if (!is_null($id)) { // null判定でからでなかったら
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if (!$itemId) { // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::availableItems()->get();
+        $products = Product::availableItems()->sortOrder($request->sort)->get();
         return view('user.index', compact('products'));
     }
 
