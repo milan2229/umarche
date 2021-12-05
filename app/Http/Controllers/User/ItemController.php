d0d5f3676bced26887cda96f40448f6d28a8381d
milan2229 <milan_1899.22@icloud.com>
2021-12-05 17:54:27 +0900

    [update] 表示件数を実装

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -30,7 +30,7 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        $products = Product::availableItems()->sortOrder($request->sort)->get();
+        $products = Product::availableItems()->sortOrder($request->sort)->paginate($request->pagination ?? '20');
         return view('user.index', compact('products'));
     }
 
