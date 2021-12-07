2b152f29b28523e5e0e2dd63d522d3a940b1a5f2
milan2229 <milan_1899.22@icloud.com>
2021-12-07 22:51:07 +0900

    [add] オーナー向けme-rusousinn

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
 
 
 class CartController extends Controller
@@ -57,14 +58,6 @@ class CartController extends Controller
 
     public function checkout()
     {
-
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('test');
-
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
@@ -113,6 +106,15 @@ class CartController extends Controller
 
     public function success()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach ($products as $product) {
+            SendOrderedMail::dispatch($product, $user);
+        }
+
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
